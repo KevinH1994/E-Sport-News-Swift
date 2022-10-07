59728e8c515449c79c97603cd5cadda5532146a1
KevinH1994 <kevinhering2@web.de>
2022-10-07 15:23:49 +0200

    Initial Commit

---
+++ E-Sport-News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Sport-News
+//
+//  Created by Kevin Hering on 07.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
